cba104e264cf40bf0af23dc760954c1f449e1e31
Archaeologist03 <omniiknight@hotmail.com>
2022-10-23 10:52:47 +0200

    Adding next-pwa (lib, manifest, logos, withPwa next config update, _document)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA({
+  // next.js config
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
